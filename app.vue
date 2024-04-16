fe1cac7e6a68c2ab20ca3160f75f630e70b4fa2c
AvetisDN <avetisdn@gmail.com>
2024-04-16 18:02:58 +0300

    layout

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+<style>
+.page-enter-active,
+.page-leave-active {
+  @apply transition-all duration-500;
+}
+.page-enter-from,
+.page-leave-to {
+  @apply opacity-0 blur-sm;
+}
+.page-enter-from {
+  @apply translate-x-24;
+}
+.page-leave-to {
+  @apply -translate-x-24;
+}
+</style>
---
+++ components/Logo.vue
@@ -0,0 +1,16 @@
+<template>
+  <NuxtLink to="/" class="flex gap-1 items-end">
+    <UIcon
+      name="i-heroicons-shopping-bag-20-solid"
+      class="h-10 w-10 text-primary-500"
+    />
+    <div class="text-2xl font-black hidden md:flex">
+      <span class="text-primary-500">PINK</span>
+      <span class="text-gray-700 dark:text-gray-100">STORE</span>
+    </div>
+  </NuxtLink>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
